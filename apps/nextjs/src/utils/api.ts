5eb8ea091c04068c38f79d03ec0d38f13215bcb7
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2023-06-17 11:36:23 -0400

    Move baseUrl to common

--- apps/nextjs/src/utils/api.ts
+++ apps/nextjs/src/utils/api.ts
@@ -3,13 +3,7 @@ import { createTRPCNext } from "@trpc/next";
 import superjson from "superjson";
 
 import type { AppRouter } from "@acme/api";
-
-const getBaseUrl = () => {
-  if (typeof window !== "undefined") return ""; // browser should use relative url
-  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
-
-  return `http://localhost:3000`; // dev SSR should use localhost
-};
+import { getBaseUrl } from "@acme/common";
 
 export const api = createTRPCNext<AppRouter>({
   config() {
--- packages/common/src/functions.ts
+++ packages/common/src/functions.ts
@@ -3,3 +3,10 @@ import { type Truthy } from "./types";
 export function isTruthy<T>(value: T): value is Truthy<T> {
   return Boolean(value);
 }
+
+export const getBaseUrl = () => {
+  if (typeof window !== "undefined") return ""; // browser should use relative url
+  if (process.env.VERCEL_URL) return `https://${process.env.VERCEL_URL}`; // SSR should use vercel url
+
+  return `http://localhost:3000`; // dev SSR should use localhost
+};
