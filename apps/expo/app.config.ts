a426941d619afab292bf5e1f13b6767622253cbf
Julius Marminge <51714798+juliusmarminge@users.noreply.github.com>
2023-07-15 20:34:34 -0400

    working POC

--- apps/expo/app.config.ts
+++ apps/expo/app.config.ts
@@ -2,6 +2,30 @@ import type { ExpoConfig } from "@expo/config";
 
 const versionCode = 5;
 
+const env = process.env.EXPO_ENVIRONMENT;
+
+/**
+ * Extend this function when going to production by
+ * setting the baseUrl to your production API URL.
+ */
+export const getBaseUrl = () => {
+  /**
+   * Gets the IP address of your host-machine. If it cannot automatically find it,
+   * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
+   * you don't have anything else running on it, or you'd have to change it.
+   */
+  const localhost =
+    // Constants.manifest?.debuggerHost?.split(":")[0] ??
+    process.env.LOCALHOST ?? "localhost";
+
+  const appUrl = env === "production" ? "" : `http://${localhost}:3000`;
+  return appUrl;
+};
+
+const APP_URL = getBaseUrl();
+const API_URL = APP_URL + "/api/trpc";
+const AUTH_URL = APP_URL + "/api/auth";
+
 const defineConfig = (): ExpoConfig => ({
   name: "t3template",
   slug: "t3template",
@@ -38,10 +62,18 @@ const defineConfig = (): ExpoConfig => ({
       // TODO PUBLISH: Change this to your own EAS project ID
       projectId: "9ae04bbe-5ef6-4ea9-b148-ac984152b1b4",
     },
-    nextAuthUrl: process.env.NEXTAUTH_URL,
-    githubId: process.env.EXPO_GITHUB_ID,
+    APP_URL,
+    API_URL,
+    AUTH_URL,
+    nextAuthUrl: AUTH_URL,
   },
-  plugins: ["expo-router", "./expo-plugins/with-modify-gradle.js", "expo-apple-authentication"],
+  // nextAuthUrl: process.env.NEXTAUTH_URL,
+  // githubId: process.env.EXPO_GITHUB_ID,
+  plugins: [
+    "expo-router",
+    "./expo-plugins/with-modify-gradle.js",
+    "expo-apple-authentication",
+  ],
 });
 
 export default defineConfig;
--- packages/auth/index.ts
+++ packages/auth/index.ts
@@ -1,3 +1,26 @@
+/**
+ * Module augmentation for `next-auth` types
+ * Allows us to add custom properties to the `session` object
+ * and keep type safety
+ * @see https://next-auth.js.org/getting-started/typescript#module-augmentation
+ */
+import { DefaultSession } from "next-auth";
+
+export type { Session } from "next-auth";
 export { authOptions } from "./src/auth-options";
 export { getServerSession } from "./src/get-session";
-export type { Session } from "next-auth";
+
+declare module "next-auth" {
+  interface Session extends DefaultSession {
+    user: {
+      id: string;
+      // ...other properties
+      // role: "ADMIN" | "USER" | "GUEST";
+    } & DefaultSession["user"];
+  }
+
+  // interface User {
+  //   // ...other properties
+  //   // role: "ADMIN" | "USER" | "GUEST";
+  // }
+}
