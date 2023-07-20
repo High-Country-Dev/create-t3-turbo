a36b8818e3a0fbfc174773ca8574e923f168850e
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2023-07-20 09:21:01 -0400

    react-admin basic implementation

---
+++ apps/nextjs/src/admin/components/Wrap.tsx
@@ -0,0 +1,11 @@
+import { type DetailedHTMLProps, type HTMLAttributes } from "react";
+
+const Wrapper = (
+  props: DetailedHTMLProps<HTMLAttributes<HTMLDivElement>, HTMLDivElement>,
+) => <div className="px-12 pt-12" {...props} />;
+
+export const Wrap =
+  <T,>(elem: (props: T) => JSX.Element) =>
+  // eslint-disable-next-line react/display-name
+  (props: T) =>
+    <Wrapper>{elem(props)}</Wrapper>;
---
+++ apps/nextjs/src/admin/resources/index.tsx
@@ -0,0 +1,1 @@
+export { default as userResource } from "./user";
