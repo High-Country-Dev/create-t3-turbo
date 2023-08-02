ec61798929b63a0f3db364e7080a783f1632bbc9
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2023-08-02 11:35:29 -0400

    Add lottie

--- apps/nextjs/src/app/page.tsx
+++ apps/nextjs/src/app/page.tsx
@@ -2,16 +2,21 @@
 
 import { Suspense } from "react";
 import { SignInButton, SignOutButton, useAuth, useUser } from "@clerk/nextjs";
+import Lottie from "lottie-react";
 
+import groovyWalkAnimation from "../assets/groovyWalk.json";
 import { CreatePostForm, PostList } from "./posts";
 
 export default function HomePage() {
   return (
     <main className="flex h-screen flex-col items-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
       <div className="container mt-12 flex flex-col items-center justify-center gap-4 px-4 py-8">
-        <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
-          Create <span className="text-pink-400">T3</span> Turbo
-        </h1>
+        <div className="flex flex-row items-center">
+          <h1 className="text-5xl font-extrabold tracking-tight sm:text-[5rem]">
+            Mountain <span className="text-pink-400">Dev</span> Template
+          </h1>
+          <Lottie animationData={groovyWalkAnimation} className="h-16 w-16" />
+        </div>
         <AuthShowcase />
 
         <CreatePostForm />
