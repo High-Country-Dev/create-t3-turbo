0621bfe27884a48c19c7109b0524d7aba017045a
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2023-08-02 11:12:25 -0400

    Add Shared package
    Better common package management

--- apps/expo/src/utils/api.tsx
+++ apps/expo/src/utils/api.tsx
@@ -17,14 +17,15 @@ export { type RouterInputs, type RouterOutputs } from "@acme/api";
  * Extend this function when going to production by
  * setting the baseUrl to your production API URL.
  */
-const getBaseUrl = () => {
+const getMobileBaseUrl = () => {
+  // TODO: use the NEXT_PUBLIC or EXPO_PUBLIC env variable here and make it optional
   /**
    * Gets the IP address of your host-machine. If it cannot automatically find it,
    * you'll have to manually set it. NOTE: Port 3000 should work for most but confirm
    * you don't have anything else running on it, or you'd have to change it.
    *
    * **NOTE**: This is only for development. In production, you'll want to set the
-   * baseUrl to your production API URL.
+   * mobileBaseUrl to your production API URL.
    */
 
   const debuggerHost = Constants.expoConfig?.hostUri;
@@ -51,7 +52,7 @@ export function TRPCProvider(props: { children: React.ReactNode }) {
       transformer: superjson,
       links: [
         httpBatchLink({
-          url: `${getBaseUrl()}/api/trpc`,
+          url: `${getMobileBaseUrl()}/api/trpc`,
           headers() {
             const headers = new Map<string, string>();
             headers.set("x-trpc-source", "expo-react");
---
+++ packages/shared/src/common/enums.ts
@@ -0,0 +1,8 @@
+export enum Header {
+  Authorization = "authorization",
+  Client = "client",
+  ContentType = "Content-Type",
+  Accept = "Accept",
+  MobileVersion = "mobile_version",
+  MobileBuild = "mobile_build",
+}
