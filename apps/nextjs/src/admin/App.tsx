989fc2ebb24d8518b9150aa1afb9163c080d914a
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2023-08-02 11:35:20 -0400

    Working react-admin posts

--- apps/nextjs/src/admin/App.tsx
+++ apps/nextjs/src/admin/App.tsx
@@ -1,19 +1,35 @@
+import { useEffect, useState } from "react";
+import Clerk from "@clerk/clerk-js";
 import { Admin, Resource } from "react-admin";
 
+import { env } from "~/env.mjs";
+import { ClerkAuthProvider } from "./providers/ClerkAuthProvider";
 import { dataProvider } from "./providers/dataProvider";
 import { queryClient } from "./providers/queryClient";
 import * as resources from "./resources";
 
 const App = () => {
-  return (
+  const [loadedClerk, setLoadedClerk] = useState<Clerk>();
+
+  useEffect(() => {
+    const clerk = new Clerk(env.NEXT_PUBLIC_CLERK_PUBLISHABLE_KEY);
+    void clerk.load().then(() => {
+      setLoadedClerk(clerk);
+    });
+  }, []);
+
+  return !loadedClerk ? null : (
     <Admin
       dataProvider={dataProvider}
       queryClient={queryClient}
-      // TODO: Implement react-admin auth with next-auth
-      // authProvider={authProvider}
-      // requireAuth={true}
+      authProvider={ClerkAuthProvider(loadedClerk, {
+        redirectOnCheckAuth: true,
+        afterSignInUrl: `${window.location.origin}/auth-callback`,
+      })}
+      requireAuth={true}
     >
-      <Resource {...resources.userResource} />
+      {/* <Resource {...resources.userResource} /> */}
+      <Resource {...resources.postResource} />
     </Admin>
   );
 };
