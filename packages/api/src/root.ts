10612a547203ed4fec5cdb7069f3666ddfa19740
Declan Nishiyama (MacBook) <declan@mountaindev.com>
2023-08-02 11:35:29 -0400

    Add a ping router

--- packages/api/src/root.ts
+++ packages/api/src/root.ts
@@ -1,10 +1,12 @@
 import { authRouter } from "./router/auth";
+import { pingRouter } from "./router/ping";
 import { postRouter } from "./router/post";
 import { webhookRouter } from "./router/webhooks";
 import { createTRPCRouter } from "./trpc";
 
 export const appRouter = createTRPCRouter({
   auth: authRouter,
+  ping: pingRouter,
   post: postRouter,
   webhook: webhookRouter,
 });
