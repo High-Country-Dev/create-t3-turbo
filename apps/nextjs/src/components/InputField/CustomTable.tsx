bdcb3866d002419731071828048088628d5f9beb
Harold Latina <haroldlatina@Harolds-MacBook-Air-2.local>
2023-08-21 12:11:10 +0800

    Add additional components

--- apps/nextjs/src/components/InputField/CustomTable.tsx
+++ apps/nextjs/src/components/InputField/CustomTable.tsx
@@ -1,75 +1,107 @@
-import React, { MouseEventHandler, useState } from "react";
+import React, { useState } from "react";
 import {
   Table,
   TableBody,
-  TableCaption,
   TableCell,
   TableHead,
   TableHeader,
   TableRow,
 } from "@/components/ui/table";
-import { format } from "date-fns";
+import type {
+  ColumnDef,
+  ColumnFiltersState,
+  SortingState,
+  VisibilityState,
+} from "@tanstack/react-table";
+import {
+  flexRender,
+  getCoreRowModel,
+  getFilteredRowModel,
+  getPaginationRowModel,
+  getSortedRowModel,
+  useReactTable,
+} from "@tanstack/react-table";
 
-export interface HeaderData {
-  label: string;
-  dataType: string;
-  clickable: boolean;
-  tooltip: string;
-  onClick?: MouseEventHandler<HTMLTableCellElement>;
+export interface CustomTableProps<TData> {
+  data: TData[];
+  columns: ColumnDef<TData>[];
 }
 
-export interface CustomTable {
-  headers: HeaderData[];
-  caption: string;
-  contents: Record<string, string | number | Date>[];
-  containerClassName: string;
-}
-
-export const CustomTable = ({
-  caption,
-  headers,
-  contents,
+export const CustomTable = <TData extends object>({
+  columns,
+  data,
   containerClassName,
-}: CustomTable) => {
+}: {
+  columns: ColumnDef<TData>[];
+  data: TData[];
+  containerClassName?: string;
+}) => {
+  const [sorting, setSorting] = useState<SortingState>([]);
+  const [columnFilters, setColumnFilters] = useState<ColumnFiltersState>([]);
+  const [columnVisibility, setColumnVisibility] = useState<VisibilityState>({});
+  const [rowSelection, setRowSelection] = useState({});
+
+  const table = useReactTable({
+    data: data,
+    columns,
+    onSortingChange: setSorting,
+    onColumnFiltersChange: setColumnFilters,
+    getCoreRowModel: getCoreRowModel(),
+    getPaginationRowModel: getPaginationRowModel(),
+    getSortedRowModel: getSortedRowModel(),
+    getFilteredRowModel: getFilteredRowModel(),
+    onColumnVisibilityChange: setColumnVisibility,
+    onRowSelectionChange: setRowSelection,
+    state: {
+      sorting,
+      columnFilters,
+      columnVisibility,
+      rowSelection,
+    },
+  });
+
   return (
     <div className={containerClassName}>
       <Table>
-        <TableCaption>{caption}</TableCaption>
         <TableHeader>
-          <TableRow>
-            {headers.map((header, index: number) => (
-              <TableHead
-                key={index}
-                className={`font-medium ${
-                  index === headers.length - 1 ? "text-right" : ""
-                }`}
-                onClick={header.clickable ? header.onClick : () => {}}
-                title={header.tooltip}
-              >
-                {header.label}
-              </TableHead>
-            ))}
-          </TableRow>
-        </TableHeader>
-        <TableBody>
-          {contents.map((content, rowIndex) => (
-            <TableRow key={rowIndex}>
-              {Object.entries(content).map(([propKey, value], columnIndex) => (
-                <TableCell
-                  key={columnIndex}
-                  className={`font-medium ${
-                    columnIndex === Object.keys(content).length - 1
-                      ? "text-right"
-                      : ""
-                  }`}
-                >
-                  {value instanceof Date
-                    ? format(value, "MMMM dd, yyyy")
-                    : value}
-                </TableCell>
-              ))}
+          {table.getHeaderGroups().map((headerGroup) => (
+            <TableRow key={headerGroup.id}>
+              {headerGroup.headers.map((header) => {
+                return (
+                  <TableHead key={header.id}>
+                    {header.isPlaceholder
+                      ? null
+                      : flexRender(
+                          header.column.columnDef.header,
+                          header.getContext(),
+                        )}
+                  </TableHead>
+                );
+              })}
             </TableRow>
           ))}
+        </TableHeader>
+        <TableBody>
+          {table.getRowModel().rows?.length ? (
+            table.getRowModel().rows.map((row) => (
+              <TableRow
+                key={row.id}
+                data-state={row.getIsSelected() && "selected"}
+              >
+                {row.getVisibleCells().map((cell) => (
+                  <TableCell key={cell.id}>
+                    {flexRender(cell.column.columnDef.cell, cell.getContext())}
+                  </TableCell>
+                ))}
+              </TableRow>
+            ))
+          ) : (
+            <TableRow>
+              <TableCell colSpan={columns.length} className="h-24 text-center">
+                No results.
+              </TableCell>
+            </TableRow>
+          )}
         </TableBody>
       </Table>
     </div>
--- apps/nextjs/src/pages/testtable.tsx
+++ apps/nextjs/src/pages/testtable.tsx
@@ -1,114 +1,91 @@
 import React from "react";
-import { z } from "zod";
+import { Button } from "@/components/ui/button";
+import type { ColumnDef } from "@tanstack/react-table";
 
-import { CustomTable, HeaderData } from "~/components/InputField/CustomTable";
-
-const FormSchema = z.object({
-  firstName: z.string(),
-  lastName: z.string(),
-  email: z.string().email(),
-});
-
-const handleHeaderClick = () => {
-  // Handle the header click logic here
-  alert("Clicked");
-};
+import { CustomTable } from "~/components/InputField/CustomTable";
 
 const TestTable = () => {
-  const headers: HeaderData[] = [
+  interface DataType {
+    id: string;
+    amount: number;
+    status: string;
+    email: string;
+  }
+  const data: DataType[] = [
     {
-      label: "Invoice",
-      dataType: "string",
-      tooltip: "Sort by Invoice",
-      clickable: true,
-      onClick: handleHeaderClick,
+      id: "m5gr84i9",
+      amount: 316,
+      status: "success",
+      email: "ken99@yahoo.com",
     },
     {
-      label: "Status",
-      dataType: "string",
-      tooltip: "Sort by Status",
-      clickable: false,
+      id: "3u1reuv4",
+      amount: 242,
+      status: "success",
+      email: "Abe45@gmail.com",
     },
     {
-      label: "Payment Method",
-      dataType: "string",
-      tooltip: "Sort by Payment Method",
-      clickable: false,
+      id: "derv1ws0",
+      amount: 837,
+      status: "processing",
+      email: "Monserrat44@gmail.com",
     },
     {
-      label: "Amount",
-      dataType: "number",
-      tooltip: "Sort by Amount",
-      clickable: false,
+      id: "5kma53ae",
+      amount: 874,
+      status: "success",
+      email: "Silas22@gmail.com",
     },
     {
-      label: "Date",
-      dataType: "number",
-      tooltip: "Sort by Amount",
-      clickable: false,
+      id: "bhqecj4p",
+      amount: 721,
+      status: "failed",
+      email: "carmella@hotmail.com",
     },
   ];
 
-  const contents = [
+  const columns: ColumnDef<DataType>[] = [
     {
-      invoice: "INV001",
-      paymentStatus: "Paid",
-      paymentMethod: "Credit Card",
-      totalAmount: "$250.00",
-      date: "August 18, 2023",
+      accessorKey: "status",
+      header: "Status",
+      cell: ({ row }) => (
+        <div className="capitalize">{row.getValue("status")}</div>
+      ),
     },
     {
-      invoice: "INV002",
-      paymentStatus: "Pending",
-      paymentMethod: "PayPal",
-      totalAmount: "$150.00",
-      date: new Date(),
+      accessorKey: "email",
+      header: ({ column }) => {
+        return (
+          <Button
+            variant="ghost"
+            onClick={() => column.toggleSorting(column.getIsSorted() === "asc")}
+          >
+            Email
+          </Button>
+        );
+      },
+      cell: ({ row }) => (
+        <div className="lowercase">{row.getValue("email")}</div>
+      ),
     },
     {
-      invoice: "INV003",
-      paymentStatus: "Unpaid",
-      paymentMethod: "Bank Transfer",
-      totalAmount: "$350.00",
-      date: new Date(),
-    },
-    {
-      invoice: "INV004",
-      paymentStatus: "Paid",
-      paymentMethod: "Credit Card",
-      totalAmount: "$450.00",
-      date: new Date(),
-    },
-    {
-      invoice: "INV005",
-      paymentStatus: "Paid",
-      paymentMethod: "PayPal",
-      totalAmount: "$550.00",
-      date: new Date(),
-    },
-    {
-      invoice: "INV006",
-      paymentStatus: "Pending",
-      paymentMethod: "Bank Transfer",
-      totalAmount: "$200.00",
-      date: new Date(),
-    },
-    {
-      invoice: "INV007",
-      paymentStatus: "Unpaid",
-      paymentMethod: "Credit Card",
-      totalAmount: "$300.00",
-      date: "",
+      accessorKey: "amount",
+      header: () => <div className="text-right">Amount</div>,
+      cell: ({ row }) => {
+        const amount = parseFloat(row.getValue("amount"));
+        const formatted = new Intl.NumberFormat("en-US", {
+          style: "currency",
+          currency: "USD",
+        }).format(amount);
+
+        return <div className="text-right font-medium">{formatted}</div>;
+      },
     },
   ];
 
   return (
     <div className="mt-50 m-auto max-w-[600px] pt-[50px] ">
-      <CustomTable
-        containerClassName="bg-blue-100"
-        caption="A list of your recent invoices."
-        contents={contents}
-        headers={headers}
-      ></CustomTable>
+      <CustomTable data={data} columns={columns}></CustomTable>
     </div>
   );
 };
